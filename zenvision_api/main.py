b1f30aa06fcdc19025eb678c243036964873ef7b
AspenLaurent <aspensolenelaurent@gmail.com>
2026-04-06 08:41:34 -0400

    security: add theveduracompany.com to CORS allowlist

--- zenvision_api/main.py
+++ zenvision_api/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://zen-vision-sigma.vercel.app", "https://vedura.co", "https://www.vedura.co", "http://localhost:3001"],
+    allow_origins=["https://zen-vision-sigma.vercel.app", "https://vedura.co", "https://www.vedura.co", "https://theveduracompany.com", "https://www.theveduracompany.com", "http://localhost:3001"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
